48fa19cb00349e8608a2eadf4693aaf818785925
Alex Tran <60246286+Ampersand-Alexander@users.noreply.github.com>
2023-08-29 19:35:16 -0700

    moved firmware from local to github

---
+++ config.h
@@ -0,0 +1,30 @@
+// Copyright 2023 Alexander Tran (@FullyAscended)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#define TAPPING_TERM 500
+#define PERMISSIVE_HOLD
+
+#define DEBUG_MATRIX_SCAN_RATE
+
+//#define QMK_WAITING_TEST_BUSY_PIN GP8
+//#define QMK_WAITING_TEST_YIELD_PIN GP9
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+//#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
+#define BACKLIGHT_PWM_DRIVER PWMD4
+#define BACKLIGHT_PWM_CHANNEL RP2040_PWM_CHANNEL_B
+
+#define AUDIO_PIN GP16
+#define AUDIO_PWM_DRIVER PWMD0
+#define AUDIO_PWM_CHANNEL RP2040_PWM_CHANNEL_A
+
+//#define ADC_PIN GP26
